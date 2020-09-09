656fb5f73283d31313dcb28eeb4252b31e69ef7c
猩猩谁来治 <oncwnuD9Vvj5-DjFTBwN3WWQ8_9I@git.weixin.qq.com>
2020-09-09 18:04:13 +0800

    遍历title调通

--- tools/detect-xmind.js
+++ tools/detect-xmind.js
@@ -7,7 +7,7 @@ const template_map_config = require("./template-map-config");
 let titles = [];
 let grades = [];
 async function getTitles(xmindFile) {
-  xml2json(xmindFile).then((result) => {
+  return xml2json(xmindFile).then((result) => {
     if (!result.code) {
       const pods = result["xmap-content"].sheet[0].topic;
       pods.forEach((item) => {
@@ -22,19 +22,22 @@ function recursion(list, grade) {
     if (Array.isArray(child.topics) && child.topics.length !== 0) {
       debugger;
       child.topics.forEach((child_topic) => {
+        debugger
         if (
           Array.isArray(child_topic.topic) &&
           child_topic.topic.length !== 0
         ) {
+          debugger
           child_topic.topic.forEach((topic_item_title) => {
+            debugger
             if (topic_item_title.title && !topic_item_title.children) {
               debugger;
-              titles[grade] = topic_item_title.title;
+              titles.push(topic_item_title.title);
               grades.push(grade);
               debugger;
             } else {
               debugger;
-              titles[grade] = topic_item_title.title;
+              titles.push( topic_item_title.title);
               debugger;
               grade += 1;
               // console.log(grade);
@@ -48,7 +51,7 @@ function recursion(list, grade) {
   });
 }
 const xmind_file = "/Users/szdt00136/Documents/测试点/测试/content.xml";
-detectXmind(xmind_file).then((result) => {
-  console.log(titles)
-  console.log(grades)
+getTitles(xmind_file).then((result) => {
+  console.log(JSON.stringify(titles));
+  console.log(grades);
 });
